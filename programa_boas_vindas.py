af21bc7139b6a9e23e6f6e976b4b61fc4728275c
Robson Paiva <103571578+Shift-ky@users.noreply.github.com>
2026-05-02 15:29:03 -0300

    Finalização da primeira atividade da faculdade

---
+++ programa_boas_vindas.py
@@ -0,0 +1,5 @@
+'''Escreva um programa que peça ao usuário que informe sua cor favorita e permita que ele digite a cor. Por fim, faça com que o programa responda exibindo o texto "Sua cor favorita é” seguido da cor digitada.'''
+
+cor_favorita = input('Digite a sua cor favorita: ')
+
+print(f'Sua cor favorito é {cor_favorita}')
