4585e5bc96cc881c3bd2e83c14f6900265a72945
Robson Paiva <103571578+Shift-ky@users.noreply.github.com>
2026-05-02 15:47:24 -0300

    Finalização da segunda atividade proposta pela facudade

---
+++ atividade 1-2.py
@@ -0,0 +1,8 @@
+'''
+Peça que o usuário digite o nome dele. Em seguida, peça o sobrenome. Mostre o texto em apenas uma linha dizendo: "Seu nome é sobrenome, nome, sobrenome", dessa maneira:
+'''
+
+nome = input('Digite o seu nome: ')
+sobrenome =  input('Digite o seu sobre nome: ')
+
+print(f'Seu nome é {nome}, {nome} {sobrenome}')
