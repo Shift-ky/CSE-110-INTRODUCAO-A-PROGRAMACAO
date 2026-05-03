510b3dea370e1af7bae58f1a50526851a5cb69dd
Robson Paiva <103571578+Shift-ky@users.noreply.github.com>
2026-05-03 08:45:46 -0300

    Atualizalção da atividade da faculdade

---
+++ história_engraçada.py
@@ -0,0 +1,27 @@
+
+'''
+S01 - Projeto: Histórias Inteligentes
+
+@Author Robson Paiva
+@date 03/05/2026 
+'''
+print('Por favor, digite o seguinte: \n')
+adjetivo = input('Adjetivo: ')  
+animal = input('Animal: ')    
+verbo1 = input('Verbo: ') 
+exclamação = input('Exclamação: ') 
+verbo2 = input('Verbo: ') 
+verbo3 = input('Verbo: ')
+
+
+
+print(f'''
+Era um dia muito {adjetivo.lower()}, quando um {animal.lower()} apareceu na porta da minha casa. Sem dizer nada, ele começou a {verbo1.lower()} no meio da sala, como se fosse a coisa mais normal do mundo.
+
+Eu fiquei tão surpreso que só consegui gritar: "{exclamação.capitalize()}!"
+
+O {animal.lower()} então parou, olhou bem nos meus olhos e decidiu {verbo2.lower()} como resposta. Antes que eu pudesse entender o que estava acontecendo, ele começou a {verbo3.lower()} pela casa inteira, derrubando tudo no caminho.
+
+No final, ninguém acreditou na minha história… mas eu sei muito bem o que vi.
+
+''')
