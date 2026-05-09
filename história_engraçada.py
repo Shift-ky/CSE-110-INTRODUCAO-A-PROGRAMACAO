6395f060775da6d912b6b515880405df5e31e03e
Robson Paiva <103571578+Shift-ky@users.noreply.github.com>
2026-05-08 22:07:32 -0300

    Atuaização da atividade

--- história_engraçada.py
+++ história_engraçada.py
@@ -9,19 +9,18 @@ print('Por favor, digite o seguinte: \n')
 adjetivo = input('Adjetivo: ')  
 animal = input('Animal: ')    
 verbo1 = input('Verbo: ') 
-exclamação = input('Exclamação: ') 
+exclamacao = input('Exclamação: ') 
 verbo2 = input('Verbo: ') 
 verbo3 = input('Verbo: ')
 
 
 
 print(f'''
-Era um dia muito {adjetivo.lower()}, quando um {animal.lower()} apareceu na porta da minha casa. Sem dizer nada, ele começou a {verbo1.lower()} no meio da sala, como se fosse a coisa mais normal do mundo.
+Sua história é:
 
-Eu fiquei tão surpreso que só consegui gritar: "{exclamação.capitalize()}!"
-
-O {animal.lower()} então parou, olhou bem nos meus olhos e decidiu {verbo2.lower()} como resposta. Antes que eu pudesse entender o que estava acontecendo, ele começou a {verbo3.lower()} pela casa inteira, derrubando tudo no caminho.
-
-No final, ninguém acreditou na minha história… mas eu sei muito bem o que vi.
-
+Outro dia, eu estava realmente em apuros. Tudo começou quando eu vi um
+{animal} {adjetivo} {verbo1} pelo corredor. Eu gritei "{exclamacao.capitalize}!". Mas tudo
+que eu conseguia pensar em fazer era {verbo2} repetidamente. Milagrosamente,
+isso fez com que ele parasse, mas não antes de tentar {verbo3}
+bem na frente da minha família.
 ''') 
