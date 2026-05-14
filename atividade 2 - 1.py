b1f2059861d38736e4b1f3414ace0eb235c9c0bf
Robson Paiva <103571578+Shift-ky@users.noreply.github.com>
2026-05-14 19:58:20 -0300

    Finalização da atividade

---
+++ atividade 2 - 1.py
@@ -0,0 +1,14 @@
+
+idade = int(input('Quantos anos você tem? '))
+
+print(f'No seu próximo aniversário, você terá {idade + 1} anos \n')
+
+ovos = int(input('Quantas caixas de ovos você tem? '))
+
+print(f'Você tem {ovos * 12} ovos \n')
+
+biscoitos = int(input('Quantos biscoitos você tem? '))
+
+pessoas = int('Quantas pessoas tem? ')
+
+print(f'Cada pessoa deve comer {biscoitos/pessoas}')
---
+++ calculadora.py
@@ -0,0 +1,39 @@
+'''
+@Author Robson Paiva
+@date 14/05/2026
+'''
+# Solicitação dos dados ao usuário
+refeicao_cianca = float(input('Qual é o preço da refeição de uma criança? '))
+refeica_aduto   = float(input('Qual é o preço da refeição de um adulto? '))
+qtd_crianca     = int(input('Há quantas crianças? '))
+qtd_adulto      = int(input('Há quantos adultos? '))
+qtb_bebidas     = int(input('Quantidade de bebida? '))
+valor_bebidas   = float(input('valor da bebida ? '))
+
+print()
+
+# Calculo para obter o subtotal
+subtotal =  (refeicao_cianca * qtd_crianca) + \
+            (refeica_aduto   * qtd_adulto)  + \
+            (valor_bebidas   * qtb_bebidas)
+
+print(f'subtotal: R$ {subtotal:.2f}')
+print()
+
+taxa = float(input('Qual é a taxa de imposto sobre vendas? '))
+
+#calculo para obter o valor da taxa de serviço
+valor_taxa = (taxa / 100) * subtotal
+
+total = valor_taxa + subtotal
+print()
+print(f'Imposto sobre Vendas: R$ {valor_taxa:.2f}')
+print(f'Total: R$ {total:.2f}')
+print()
+
+pagamento = float(input('Qual é o valor do pagamento? '))
+
+#Calculo do troco
+troco = pagamento - total
+print()
+print(f'Troco: R$ {troco:.2f}')
